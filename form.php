b20e87fb307d9cc5d061d92cef8b22eddccafffd
Sardar219 <sardarhossaini2022@gmail.com>
2024-07-24 13:55:20 +0430

    add the upload image from every where

--- form.php
+++ form.php
@@ -4,7 +4,14 @@ require __DIR__ . '/inc/dbcon.inc.php';
 $title=$_POST['title'] ?? null;
 $date=$_POST['date'] ?? null;
 $message=nl2br($_POST['message'] ?? null);
-$image_name=$_POST['image_name'] ?? null;
+$image_name=$_FILES['image_name']["name"] ?? null;
+var_dump($_FILES);
+if(!empty($_FILES) && !empty($_FILES["image_name"])){
+  if($_FILES["image_name"]["error"] === 0 && $_FILES["image_name"]["size"] !== 0){
+    move_uploaded_file($_FILES['image_name']['tmp_name'],__DIR__."/images/".time().".jpg");
+  }
+}
+var_dump($image_name);
 if(!empty($_POST)){
   $stem=$pdo->prepare('INSERT INTO `entires` (`id`, `title`, `message`, `date`, `image_name`) VALUES (NULL, :title, :message, :date,:image_name );');
   $stem->bindValue("title",$title);
@@ -20,7 +27,7 @@ if(!empty($_POST)){
     <main>
       <div class="container">
         <h1 class="main-header">New Entry</h1>
-        <form action="form.php" method="POST">
+        <form action="form.php" method="POST" enctype="multipart/form-data">
           <div class="form-group">
             <label for="title" class="form-group-label">Title:</label>
             <input
